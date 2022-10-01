5cf7d8267a4ec95ebbff6a8bfabc734e5f40b105
vsm <devops.july2.2017@gmail.com>
2022-10-01 10:02:50 +0530

    Update cluster-pre-setup.sh

--- cluster-pre-setup.sh
+++ cluster-pre-setup.sh
@@ -23,3 +23,9 @@ curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add
 cat <<EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list
 deb https://apt.kubernetes.io/ kubernetes-xenial main
 EOF
+   
+   
+   apt-get update && apt-get install -y kubelet kubeadm kubectl && apt-mark hold kubelet kubeadm kubectl
+
+   systemctl daemon-reload && systemctl enable kubelet && systemctl restart kubelet && systemctl status kubelet
+   
