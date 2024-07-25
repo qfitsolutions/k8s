42e83fe98c94745cc809ce9ea98421dbfac0453c
vsm <devops.july2.2017@gmail.com>
2024-07-25 13:20:54 +0530

    Update eksctl.sh

--- eksctl.sh
+++ eksctl.sh
@@ -20,3 +20,4 @@ chmod 777 kubectl
 mv kubectl /usr/local/bin/
 
 #kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.7.0/aio/deploy/recommended.yaml
+#https://github.com/kubernetes/dashboard/blob/master/docs/user/access-control/creating-sample-user.md
